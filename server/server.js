1c86b3d90ebd3f782f8eb170436a55de33dec7cc
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-21 18:24:53 -0700

    Simplified server, serves bundled files when request to / is made

--- server/server.js
+++ server/server.js
@@ -1,32 +1,12 @@
-const path = require('path');
 const express = require('express');
-
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+const path = require('path');
 
-// app.use('/static', express.static('client'));
-app.get('/', (req, res) => res.sendFile('../client/index.html'));
+app.use('/', express.static(path.join(__dirname, '../dist')));
+// app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, '../client/index.html')));
 
-// catch-all requests to unknown routes
-app.use((req, res) => res.sendStatus(404));
+app.use('/', (req, res) => res.send('yo yo yo'));
 
-app.use((err, req, res, next) => {
-    const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 500,
-        message: {
-            err: 'An error occurred'
-        }
-    };
-
-    const errObj = Object.assign(defaultErr, err);
-    console.log(errObj.log);
-
-    return res.status(errObj.status).json(errObj.message);
-});
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-module.exports = app;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
