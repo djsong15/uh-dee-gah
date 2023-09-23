ca9df839c1aae7f728c62e9ff478980bc4036824
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-22 23:00:17 -0700

    Create first reducer

---
+++ client/reducers/locations.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    totalLocsVisited: 0,
+    totalLocsToVisit: 0,
+    locList: [],
+    lastLocId: 100
+}
+
+export const locSlice = createSlice({
+    name: 'locations',
+    initialState,
+    reducers: {
+        addLocation: (state, action) => {
+            ++state.totalLocsToVisit;
+            state.locList.push({
+                locId: ++state.lastLocId,
+                location: action.payload,
+                visited: false
+            });
+        },
+        visitedLoc: (state, action) => {
+            --state.totalLocsToVisit;
+            ++state.totalLocsVisited;
+            for (const loc of state.locList) {
+                if (action.payload === loc.locId) {
+                    loc.visited = true;
+                }
+            }
+        }
+    }
+});
+
+export const { addLocation, visitedLoc } = locSlice.actions;
+export default locSlice.reducer;
