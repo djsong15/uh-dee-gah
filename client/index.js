eb38a491ac0ebf19343813c5ec09cd4fd918548b
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-22 22:59:44 -0700

    Set up RTK; import styles

--- client/index.js
+++ client/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { createRoot } from 'react-dom';
+
 import App from "./App.jsx";
+import styles from './app.scss';
+import { store } from "./store.js";
+import { Provider } from "react-redux";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
