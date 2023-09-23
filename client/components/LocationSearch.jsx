390e791553d85b205309f0a8b193b0b31bb0b116
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-23 14:01:18 -0700

    LocationSearch renders a combobox with autocomplete from the Places library

--- client/components/LocationSearch.jsx
+++ client/components/LocationSearch.jsx
@@ -1,30 +1,46 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 import { addLocation } from "../reducers/locations";
-import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng
-} from "use-places-autocomplete";
-import { Autocomplete, TextField } from "@mui/material";
+import { StandaloneSearchBox } from "@react-google-maps/api";
 
 export default function LocationSearch() {
-    const {
-        ready,
-        value,
-        setValue,
-        suggestions: { status, data },
-        clearSuggestions
-    } = usePlacesAutocomplete();
-    const testOptions = [
-        'Los Angeles',
-        'San Francisco'
-    ];
-
+    const inputRef = useRef();
+    const dispatch = useDispatch();
+    const placeChangeHandler = () => {
+        const [place] = inputRef.current.getPlaces();
+        if (place) {
+            console.log(place);
+            // console.log(place.geometry.location.lng());
+        }
+        console.log(inputRef.current);
+    }
+        
+    
+    console.log(inputRef);
     return (
-        <Autocomplete 
-            id="location-combobox"
-            options={testOptions}
-            renderInput={(params) => <TextField {...params} label="places" />}
-        />
-    )
+        <>
+            <StandaloneSearchBox
+                onLoad={ref => inputRef.current = ref}
+                onPlacesChanged={placeChangeHandler}
+                options={{
+                    fields: ['geometry', 'name']
+                }}
+            >
+                <input
+                    id="location-combobox"
+                    type="text"
+                    placeholder="Where to?"
+                    autoFocus
+                />
+            </StandaloneSearchBox>
+            <button
+                onClick={() => {
+                    if (inputRef.current !== '') {
+                        const [place] = inputRef.current.getPlaces();
+                    }
+                    if (place) dispatch(addLocation(place.name));
+                }}
+            >Submit</button>
+        </>
+    );
 };
