2f9c69c6b3ec4ae775bd180ef46397115f24ddb2
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-22 22:59:12 -0700

    Set up RTK; import styles

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import MainContainer from "./MainContainer.jsx";
 
 export default function App() {
     return (
         <div>
-            <h1>I'm a component, boi</h1>
+            <h1>I'm the App, boi</h1>
+            <MainContainer />
         </div>
     );
 }
