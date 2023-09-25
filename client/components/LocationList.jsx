533abd13e2f9c389e73be5ef2361a282bda4aff1
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-25 08:16:51 -0700

    Refactored LocationList component, no longer stores list rendering func into its own function in memory

--- client/components/LocationList.jsx
+++ client/components/LocationList.jsx
@@ -1,31 +1,26 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Location from "./Location.jsx";
-
-const LocList = function ({ locList }) {
-    if (!locList.length) return;
-    return (
-        <div>
-            {locList.map((el ,i) => {
-                return <Location
-                    key={i}
-                    locId={el.locId}
-                    location={el.location}
-                    visited={() => el.visited ? 'Yes' : 'No'}
-                />;
-            })}
-        </div>
-    );
-}
 
 export default function LocationList() {
     const locList = useSelector(state => state.locations.locList);
 
     return (
-        <div id="locList">
-            <h4>Places</h4>
-            <LocList locList={locList}/>
+        <div id="loc-list">
+            <h3>Places</h3>
+            {locList.length ? (
+                <div>
+                    {locList.map((el ,i) => {
+                        return <Location
+                            key={i}
+                            locId={el.locId}
+                            location={el.location}
+                            visited={el.visited}
+                        />;
+                    })}
+                </div>
+            ) : <div></div>}
         </div>
     );
 }
