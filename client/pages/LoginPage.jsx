ae794f2fa9d50e19ac080c4f2e82a0b0417dc3db
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-27 18:09:13 -0700

    Create Login and Signup pages

--- client/pages/LoginPage.jsx
+++ client/pages/LoginPage.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, NavLink } from "react-router-dom";
+import SignupPage from "./SignupPage";
 
 export default function LoginPage() {
     return (
-        <form action="/login" method="POST">
-            <input name="username" type="text" placeholder="username" />
-            <input name="password" type="text" />
-            <input type="submit" />
-        </form>
+        <>
+            <h2>Welcome back!</h2>
+            <form action="/login" method="POST">
+                <input name="username" type="text" placeholder="username" />
+                <input name="password" type="text" />
+                <input type="submit" />
+            </form>
+            <h3>Don't have an account?
+                <BrowserRouter><NavLink to='/signup'>
+                    Sign up here
+                </NavLink></BrowserRouter>
+            </h3>
+        </>
     );
 }
