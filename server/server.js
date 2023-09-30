b74a530922297d6b9418f4e84401c12195907840
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-09-30 13:13:07 -0700

    Server connects to MongoDB; routes have also been updated

--- server/server.js
+++ server/server.js
@@ -3,10 +3,41 @@ const app = express();
 const PORT = 3000;
 
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
-app.use('/', express.static(path.join(__dirname, '../dist')));
-// app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, '../client/index.html')));
+const sessionController = require('./controllers/sessionController');
 
-app.use('/', (req, res) => res.send('yo yo yo'));
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'udg-users'
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+const rootRouter = require('./routes/rootRouter');
+const dbRouter = require('./routes/dbRouter');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// app.use('/', express.static(path.join(__dirname, '../dist')));
+
+app.use('/db', dbRouter);
+app.use('/', rootRouter);
+
+// app.get('/',
+//     // sessionController.isLoggedIn,
+//     (req, res) => {
+//         return res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+//     }
+// );
+
+app.use((req, res) => res.sendStatus(404));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+module.exports = app;
