eff5d0e916306a3d8a9bafa78d1c1409e04bc7fd
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-10-02 16:53:30 -0700

    Login page verifies account in DB before logging one into the app

--- client/pages/LoginPage.jsx
+++ client/pages/LoginPage.jsx
@@ -1,21 +1,48 @@
-import React from "react";
-import { BrowserRouter, NavLink } from "react-router-dom";
-import SignupPage from "./SignupPage";
+import React, { useState } from "react";
+import { NavLink, redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logIn } from "../reducers/user";
 
 export default function LoginPage() {
+    const [ username, setUsername ] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch(logIn(username));
+        // dispatch(loggedIn(username));
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: e.target.username.value,
+                password: e.target.password.value
+            })
+        });
+        // console.log(response);
+        redirect('/');
+    }
+
     return (
-        <>
+        <div>
             <h2>Welcome back!</h2>
-            <form action="/login" method="POST">
-                <input name="username" type="text" placeholder="username" />
-                <input name="password" type="text" />
+            <form action="/login" method="POST" onSubmit={handleSubmit}>
+                <input 
+                    name="username"
+                    type="text"
+                    placeholder="username"
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input name="password" type="password" />
                 <input type="submit" />
             </form>
             <h3>Don't have an account?
-                <BrowserRouter><NavLink to='/signup'>
+                <NavLink to='/signup'>
                     Sign up here
-                </NavLink></BrowserRouter>
+                </NavLink>
             </h3>
-        </>
+        </div>
     );
 }
