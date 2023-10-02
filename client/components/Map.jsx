8c7323f8d44607078a0d8f575f3a1fef8956ee54
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-10-02 16:50:43 -0700

    Added second option for visited locations; map does not re-render when a locatin's visited value is toggled

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,9 +1,15 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
-export default function Map() {
-    const locList = useSelector(state => state.locations.locList);
+export default function Map({ setMap }) {
+    const locationsState = useSelector(state => state.locations);
+    const { locList, totalLocsVisited } = locationsState;
+    
+    const onMapLoad = (map) => {
+        console.log('render map comp', totalLocsVisited);
+        setMap(map);
+    };
 
     const center = useMemo(() => ({
         lat: 34.0549,
@@ -12,25 +18,37 @@ export default function Map() {
     const options = useMemo(() => ({
         mapTypeControl: false,
         streetViewControl: false,
-        fullscreenControl: false
+        fullscreenControl: false,
+        minZoom: 3
     }), []);
 
+    const visitedOptions = {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+        fillColor: 'green',
+        strokeColor: 'lightgreen',
+        fillOpacity: 1.0
+    };
+    const unvisitedOptions = {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+        fillColor: 'red',
+        strokeColor: 'pink',
+        fillOpacity: 1.0
+    }
+
     return <GoogleMap
-        zoom={10}
+        zoom={3}
         center={center}
         options={options}
-        mapContainerClassName="map-container"
+        id="map-container"
+        onLoad={onMapLoad}
     >
         {locList.length && locList.map((el) => {
             return <MarkerF
                 key={el.locId}
                 position={el.geoPos}
-                options={{
-                    icon: {
-                        fillColor: '#14c214'
-                    }
-                }}
+                icon={el.visited ? visitedOptions : unvisitedOptions}
             />
         })}
     </GoogleMap>;
 }
+
