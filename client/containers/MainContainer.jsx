8da3e019c29202c033662b499183da815c6d8b01
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-10-02 16:48:51 -0700

    Store map render in state

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector.js";
+import { useSelector } from "react-redux/es/hooks/useSelector.js";
 import { useLoadScript } from "@react-google-maps/api";
 
 import Map from "../components/Map.jsx";
@@ -8,23 +8,18 @@ import LocContainer from "./LocContainer.jsx";
 const libraries = ['places', 'marker'];
 
 export default function MainContainer() {
-    // const locList = useSelector(state => state.locations.locList);
-    const [ mSwitch, setMSwitch ] = useState(false);
-    const flipMSwitch = () => setMSwitch(mSwitch ? false : true);
-    console.log('MainContainer state', mSwitch);
-
+    const [ map, setMap ] = useState(null);
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLEMAPS_API_KEY,
         libraries: libraries
     });
 
     return (
-        <>
-            <div className="container">
-                <h2>i'm a container, son</h2>
-                {isLoaded ? <Map /> : <div>Loading...</div>}
-            </div>               
-            <LocContainer flipMSwitch={flipMSwitch} isLoaded={isLoaded}/>
-        </>
+        <div id="main-container">
+            <LocContainer isLoaded={isLoaded}/>
+            <div id="map-container">
+                {isLoaded ? <Map setMap={setMap}/> : <div>Loading...</div>}
+            </div>  
+        </div>
     );
 }
