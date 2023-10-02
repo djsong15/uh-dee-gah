be016099c7a2ccd562c9fb684fe2d83335263160
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-10-02 16:54:05 -0700

    Signup page allows one to create a new account which gets stored in the DB. Then proceeds to log user in with the new account

--- client/pages/SignupPage.jsx
+++ client/pages/SignupPage.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { redirect } from "react-router-dom";
+import { logIn } from "../reducers/user";
 
 export default function SignupPage() {
+    const [ username, setUsername ] = useState('');
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log('blah ', e.target.username.value);
+        // setUsername(e.target.username.value);
+        // console.log('state is now', username);
+        fetch('/signup', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: e.target.username.value,
+                password: e.target.password.value
+            })
+        })
+            .then(() => dispatch(logIn(username)))
+            .catch(err => {
+                console.log(JSON.stringify(err));
+                return undefined;
+            });
+        // console.log(response);
+        // dispatch(logIn(username));
+        redirect('/');
+    }
+
     return (
-        <form action="/signup" method="POST">
-            <input name="username" type="text" placeholder="username" />
-            <input name="password" type="text" />
-            <input type="submit" />
-        </form>
+        <div>
+            <h2>Fill in the details below to create an account</h2>
+            <form action="/signup" method="POST" onSubmit={handleSubmit}>
+                <input 
+                    name="username"
+                    type="text"
+                    placeholder="username"
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input name="password" type="password" autoComplete="off"/>
+                <input type="submit" />
+            </form>
+        </div>
     );
 }
